f53b87c45a1de592108134b4b772ce29e04a6525
marvintau <nightvista@gmail.com>
2020-02-13 19:09:32 +0800

    for re-clone

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
-
+import List from 'formwell-viewport';
 import Refcell, {Evaluatable} from './Refcell';
 import {genCascadedNameEntries} from './nameGenerate';
 
 const nameEntries = genCascadedNameEntries(100);
 console.log(nameEntries);
+
+const ColRenderer = ({children}) => 
+  <div style={{margin:'0.5rem'}}>{children}</div>;
+
+const HistColRenderer = ({children}) =>
+  <div style={{margin:'0.5rem'}}>{children}</div>;
+
+const HeaderColRenderer = ({children}) =>
+  <div style={{margin:'0.5rem'}}>{children}</div>;
+
+const colSpecs = {
+  name: {desc: '名称', width: 2, isSortable: false, isFilterable: false, ColRenderer, HistColRenderer, HeaderColRenderer},
+  desc: {desc: '描述', width: 6, isSortable:  true, isFilterable:  true, ColRenderer, HistColRenderer, HeaderColRenderer},
+  mb: {desc: '期初', width: 1, isSortable:  true, isFilterable:  true, ColRenderer, HistColRenderer, HeaderColRenderer},
+  mc: {desc: '贷方', width: 1, isSortable:  true, isFilterable:  true, ColRenderer, HistColRenderer, HeaderColRenderer},
+  md: {desc: '借方', width: 1, isSortable:  true, isFilterable:  true, ColRenderer, HistColRenderer, HeaderColRenderer},
+  me: {desc: '期末', width: 1, isSortable:  true, isFilterable:  true, ColRenderer, HistColRenderer, HeaderColRenderer},
+}
+
 
 const table = [
   {desc: 'example', value: 'exmaple1', children: []},
@@ -23,5 +42,6 @@ export default () => {
     <Evaluatable {...{table, referredTable:nameEntries, pathColumn:'desc', evalColumnDict}}>
       {table.map((rec, i) => <Refcell key={i} path={[i]} disabled={false} />)}
     </Evaluatable>
+    <List data={nameEntries} colSpecs={colSpecs} />
   </div>
 }
